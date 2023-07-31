9765cb2da7dd228df1ebc81bb60dacfb7906141c
Sebastian Zambrano <50499778+SebastianZambranoR@users.noreply.github.com>
2023-07-31 11:06:32 -0500

    Update PlayerGamePadAssigner.cs

--- Assets/Script/Players/PlayerInputs/PlayerGamePadAssigner.cs
+++ Assets/Script/Players/PlayerInputs/PlayerGamePadAssigner.cs
@@ -19,11 +19,13 @@ public class PlayerGamePadAssigner : MonoBehaviour
     private void Awake()
     {
         _inputManager = GetComponent<PlayerInputManager>();
+        _inputManager.onPlayerJoined += CheckInputs;
     }
 
     private void Start()
     {
-
+        /*
+        Debug.Log(Gamepad.all.Count);
         if (Gamepad.all.Count > 1)
         {
             _playerInput1.user.UnpairDevices();
@@ -36,8 +38,33 @@ public class PlayerGamePadAssigner : MonoBehaviour
             _playerInput1.user.UnpairDevice(Gamepad.all[0]);
             _playerInput2.user.UnpairDevices();
             InputUser.PerformPairingWithDevice(Gamepad.all[0], _playerInput2.user);
+        }*/
+    }
+
+    public void CheckInputs(PlayerInput _playerInput)
+    {
+        
+        if (Gamepad.all.Count == 1)
+        {
+            if (_playerInput.gameObject.name == "Levana_Animated")
+            {
+                _playerInput.user.UnpairDevice(Gamepad.all[0]);
+            }
         }
 
-
+        if (Gamepad.all.Count == 2)
+        {
+            if (_playerInput.gameObject.name == "Levana_Animated")
+            {
+                _playerInput.user.UnpairDevices();
+                InputUser.PerformPairingWithDevice(Gamepad.all[0], _playerInput.user);
+            }
+            
+            if (_playerInput.gameObject.name == "Soleil_Animated")
+            {
+                _playerInput.user.UnpairDevices();
+                InputUser.PerformPairingWithDevice(Gamepad.all[1], _playerInput.user);
+            }
+        }
     }
 }
